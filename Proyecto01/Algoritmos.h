9ae05f43363ac8e941a3482f18955e20886b89cb
space_samurai707 <thicchongus@gmail.com>
2026-06-15 00:28:08 -0600

    primer commit

---
+++ Proyecto01/Algoritmos.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Grafo.h"
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <functional>
+class Algoritmos
+{};
+
---
+++ Proyecto01/Grafo.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Nodo.h"
+#include <vector>
+#include <string>
+class Grafo
+{};
+

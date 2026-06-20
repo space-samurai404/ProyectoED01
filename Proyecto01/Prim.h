d040670bb4809cdf3c40af08e9e8a655d572dccf
Bryan <bryan032510@gmail.com>
2026-06-20 01:24:55 -0600

    se crea la clase prim

---
+++ Proyecto01/Prim.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "Grafo.h"
+#include "Stack.h"
+#include "ArrayStack.h"
+
+class Prim {
+private:
+	Grafo* grafo;
+
+public:
+	Prim(Grafo* grafo)
+	{
+		this->grafo = grafo; 
+	}
+
+	Grafo hacerRecorrido(int primerNodo){
+		
+		int nodo = grafo->numNodos(); 
+		Grafo aNuevo(grafo->dirigido);
+
+
+		for (int i = 0; i < nodo; i++){
+			aNuevo.agregarNodo(grafo->posiciones[i].x, grafo->posiciones[i].y); 
+		}
+
+		bool* nodosVisitados = new bool[nodo];
+
+		for (int i = 0; i < nodo; i++){
+			nodosVisitados[i] = false; 
+		}
+
+		ArrayStack<int> pila(nodo); 
+
+		nodosVisitados[primerNodo] = true;
+		pila.push(primerNodo);
+
+
+		while (!pila.isEmpty()) {
+			int minPeso = -1;
+			int origen = -1;
+			int destino = -1;
+
+			for (int i = 0; i < nodo; i++) {
+				if (nodosVisitados[i]) {
+					for (int j = 0; j < nodo; j++) {
+						if (!nodosVisitados[j] && grafo->hayArista(i, j)) {
+							int peso = grafo->pesoArista(i, j);
+							if (minPeso == -1 || peso < minPeso) {
+								minPeso = peso;
+								origen = i;
+								destino = j;
+							}
+						}
+					}
+				}
+			}
+			
+			nodosVisitados[destino] = true;
+			aNuevo.agregarArista(origen, destino, minPeso);
+			visitados++;
+		}
+		delete[] nodosVisitados;
+		return aNuevo; 
+
+	}
+};

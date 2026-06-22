a9ff8e872aaf585cccb7ff316baee86857edd76c
KZ14-debug <kendallzamora2@gmail.com>
2026-06-21 21:11:16 -0600

    Refactor DFS class and improve traversal logic

--- Proyecto01/DFS.h
+++ Proyecto01/DFS.h
@@ -53,7 +53,7 @@ public:
 		{
 
 			int nodoActual = pila.pop(); //Esto es para sacar el nodo que se encuentra en la cima de la pila
-			int aristaMedida = grafo->pesoArista(nodoActual, nodoVecino);
+			
 
 			for (int nodoVecino = nodo - 1; nodoVecino >= 0;nodoVecino--) //Esto es para recorrer los nodos vecinos 
 			{
@@ -63,7 +63,9 @@ public:
 
 					nodosVisitados[nodoVecino] = true;
 
+					int aristaMedida = grafo->pesoArista(nodoActual, nodoVecino);
 
+					
 					aNuevo.agregarArista(nodoActual, nodoVecino, aristaMedida); // Esto es para agregar la arista al nuevo grafo, para asi construir el arbol de expasion 
 
 					pila.push(nodoVecino); // Esto es para agregar el nodo vecino a la pila para el recorrido
